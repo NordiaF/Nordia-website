9da21e5d16756041bbef768c1ead642064c679c2
O-tega <tega.myview@gmail.com>
2026-04-09 22:35:56 +0100

    feat: updated the url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,7 +9,7 @@ const routes = [
   "/events-and-outreaches",
   "/contact",
   "/blog",
-  "/power-of-productivity",
+  "/power-your-productivity",
 ];
 
 export default function sitemap(): MetadataRoute.Sitemap {
